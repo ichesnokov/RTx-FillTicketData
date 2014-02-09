c5d5924e0214e878caddf5331afc8e5d7ffe477d
Ilya Chesnokov <chesnokov.ilya@gmail.com>
2014-02-09 17:23:00 -0500

    Added read_config() and init_connections()

--- lib/RTx/AutofillCustomFields.pm
+++ lib/RTx/AutofillCustomFields.pm
@@ -2,8 +2,76 @@ use strict;
 use warnings;
 
 package RTx::AutofillCustomFields;
+
 our $VERSION = '0.01';
 
+use DBI;
+use Digest::MD5 qw(md5_hex);
+use JSON;
+
 RT->AddJavaScript('RTx-AutofillCustomFields.js');
+
+my $old_md5_sum = ''; # avoid uninitialized warning
+
+my $config;
+sub read_config {
+    my $config_file = shift;
+
+    # Reading file
+    local $/;
+    open my $FH, '<', $config_file
+        or die "Could not open file $config_file: $!";
+    my $json_data = <$FH>;
+    close $FH;
+
+    my $md5_sum = md5_hex($json_data);
+    if ($old_md5_sum eq $md5_sum) {
+        RT->Logger->debug("md5 sum matches the old one ($md5_sum), not re-reading config");
+        return $config;
+    }
+    RT->Logger->debug("new plugin configuration detected");
+
+    $config = JSON::from_json($json_data);
+}
+
+my %dbh_for;
+
+# Re-initialize database connections
+sub init_connections {
+    undef %dbh_for;
+
+    for my $db_id (keys %{ $config->{databases} }) {
+        $dbh_for{$db_id} = _connect_db($config->{databases}->{$db_id});
+    }
+}
+
+# Connect to a database using configuration from $db_config
+sub _connect_db {
+    my $db_config = shift;
+
+    my $db_type = lc $db_config->{type};
+    my $dsn = "dbi:$db_type:$db_config->{database}";
+    for my $field (qw(host port)) {
+        $dsn .= ";$field=$db_config->{$field}" if $db_config->{$field};
+    }
+
+    my %more_attrs =
+        $db_type eq 'mysql' ? ( mysql_enable_utf8 => 1 )
+      : $db_type eq 'pg'    ? ( pg_enable_utf8    => 1 )
+      :                       ();
+
+    my $dbh = DBI->connect(
+        $dsn,
+        $db_config->{user},
+        $db_config->{password},
+        {
+            RaiseError => 1,
+            PrintError => 1,
+            %more_attrs
+        },
+    );
+    $dbh->do('SET NAMES utf8');
+    return $dbh;
+}
 
 1;
