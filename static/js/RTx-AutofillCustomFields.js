07fcccab338b3ed69945298f3242976faf145706
Ilya Chesnokov <chesnokov.ilya@gmail.com>
2014-02-09 14:54:37 -0500

    A bit better js

--- static/js/RTx-AutofillCustomFields.js
+++ static/js/RTx-AutofillCustomFields.js
@@ -1,10 +1,10 @@
 jQuery(function($) {
-    var update_field_value = function(field) {
+    var update_field_value = function(field, key_field_value) {
         var ids = $(field).attr('class').match(/(\d+)/);
         var field_id = ids[0];
         if (field_id) {
             //jQuery.ajax('/Elements/AutoFillCustomFields', {
-            //    data: { key_field_id: field_id },
+            //    data: { field_id: field_id, key_field_value: key_field_value },
             //    dataType: 'json',
             //    success: function(data) {
             //        $(field).val(data.value);
@@ -15,7 +15,7 @@ jQuery(function($) {
     };
     var autofill_custom_fields = function() {
         var custom_fields = jQuery("[class*=CF-]").each(function(){
-            update_field_value(this);
+            update_field_value(this, key_field_value);
         });
     };
     jQuery('#autofill_custom_fields').click(function(ev){
