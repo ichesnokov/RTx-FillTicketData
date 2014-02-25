fa329b58901759839b4aae802a9ada12aec40b5a
Ilya Chesnokov <chesnokov.ilya@gmail.com>
2014-02-25 13:34:58 +0400

    Updated sample configs

--- etc/RT_SiteConfig_sample.pm
+++ etc/RT_SiteConfig_sample.pm
@@ -1,3 +1,3 @@
+# Lines to add to RT_SiteConfig.pm
 Plugin('RTx::FillTicketData');
-Set( $FillTicketDataSettingsFile =>
-     '/home/user/rt4/etc/fill_ticket_data_settings.json' );
+Set($FillTicketDataSettingsFile => '/home/rt/rt4/etc/fill_ticket_data_settings.json');
